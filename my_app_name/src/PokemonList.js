73bad2a79b327a3c9b42d51dd1411d1677b430ea
markchoward7 <markchoward7@outlook.com>
2020-06-17 16:21:22 -0500

    fixed merge conflict

--- my_app_name/src/PokemonList.js
+++ my_app_name/src/PokemonList.js
@@ -1,17 +1,4 @@
 import React from 'react'
-<<<<<<< HEAD
-import Pokemon from './Pokemon'
-
-function PokemonList(pokedex) {
-  const pokeList = pokedex.map((pokemon, index) => 
-  { return ( <Pokemon pokemon = {pokemon}/>)
-  });
-
-  return (<div> {PokemonList} </div>)
-
-  export default PokemonList
-=======
-
 import Pokemon from './Pokemon'
 
 function PokemonList({listOfPokemon, parent}) {
@@ -23,4 +10,3 @@ function PokemonList({listOfPokemon, parent}) {
 }
 
 export default PokemonList
->>>>>>> 70557caa5d1a547c47428513e0d4461dfaf309cb
--- my_app_name/src/first/student.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function Student({student}) {
-    return <li>Student ID: {student.id}, Name: {student.name}, Class: {student.class}, Location: {student.location}, Role: {student.role} </li>
-}
-
-export default Student
