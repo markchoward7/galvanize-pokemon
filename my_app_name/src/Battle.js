173b141847a92f7edd755559216783f904d0f544
markchoward7 <markchoward7@outlook.com>
2020-06-18 11:32:38 -0500

    #18, type advantage

--- my_app_name/src/Battle.js
+++ my_app_name/src/Battle.js
@@ -59,19 +59,107 @@ function DetermineWinner(pokemon1, pokemon2) {
     return battleValue > 0
 }
 
+const typeAdvantages = {
+    normal: [],
+    fire: ["grass", "ice", "bug", "steel"],
+    water: ["fire", "ground", "rock"],
+    electric: ["water", "flying"],
+    grass: ["water", "ground", "rock"],
+    ice: ["grass", "ground", "flying", "dragon"],
+    fighting: ["normal", "ice", "rock", "dark", "steel"],
+    poison: ["grass"],
+    ground: ["fire", "electric", "poison", "rock", "steel"],
+    flying: ["grass", "fighting", "bug"],
+    psychic: ["fighting", "poison"],
+    bug: ["grass", "psychic", "dark"],
+    rock: ["fire", "ice", "ground", "psychic"],
+    ghost: ["psychic", "ghost"],
+    dragon: ["dragon"],
+    dark: ["psychic", "ghost"],
+    steel: ["ice", "rock"]
+}
+const typeDisadvantages = {
+    normal: ["rock", "steel"],
+    fire: ["fire", "water", "rock", "dragon"],
+    water: ["water", "grass", "dragon"],
+    electric: ["electric", "grass", "dragon"],
+    grass: ["fire", "grass", "poison", "flying", "bug", "dragon", "steel"],
+    ice: ["fire", "water", "ice", "steel"],
+    fighting: ["poison", "flying", "psychic", "bug"],
+    poison: ["poison", "ground", "rock", "ghost"],
+    ground: ["grass", "bug"],
+    flying: ["electric", "rock", "steel"],
+    psychic: ["psychic", "steel"],
+    bug: ["fire", "fighting", "poison", "flying", "ghost", "steel"],
+    rock: ["fighting", "ground", "steel"],
+    ghost: ["dark", "steel"],
+    dragon: ["steel"],
+    dark: ["fighting", "dark", "steel"],
+    steel: ["fire", "water", "electric", "steel"]
+}
+const typeImmunities = {
+    normal: ["ghost"],
+    fire: [],
+    water: [],
+    electric: ["ground"],
+    grass: [],
+    ice: [],
+    fighting: ["ghost"],
+    poison: ["steel"],
+    ground: ["flying"],
+    flying: [],
+    psychic: ["dark"],
+    bug: [],
+    rock: [],
+    ghost: ["normal"],
+    dragon: [],
+    dark: [],
+    steel: []
+}
+
 function GetTypeAdvantage(pokemon1, pokemon2) {
     let advantageValue = 0
-
-    // if 2x supereffective
-    // advantageValue += 100
-    // if supereffective
-    // advantageValue += 50
-    // if effective
-    // no change
-    // if resisted
-    // advantageValue -= 25
-    // if 2x resisted
-    // advantageValue -= 75
+    
+    for (const poke1Type of pokemon1.types) {
+        let superEffective = 0
+        for (const poke2Type of pokemon2.types) {
+            if (typeAdvantages[poke1Type.type.name].includes(poke2Type.type.name)) {
+                superEffective++
+            }
+        }
+        if (superEffective === 2) {
+            advantageValue = 100
+            break
+        } else if (superEffective === 1) {
+            advantageValue = 50
+        }
+    }
+    
+    if (advantageValue === 0) {
+        for (const poke1Type of pokemon1.types) {
+            let notEffective = 0
+            for (const poke2Type of pokemon2.types) {
+                if (typeDisadvantages[poke1Type.type.name].includes(poke2Type.type.name)) {
+                    notEffective++
+                }
+            }
+            if (notEffective === 0) {
+                advantageValue = 0
+                for(const poke2Type of pokemon2.types) {
+                    if (typeImmunities[poke1Type.type.name].includes(poke2Type.type.name)) {
+                        advantageValue = -1000
+                    }
+                }
+                if (advantageValue === 0) {
+                    break
+                }
+            } else if(notEffective === 1) {
+                advantageValue = -25
+            } else if (advantageValue === 0 || advantageValue === -1000) {
+                advantageValue = -75
+            }
+        }
+    }
 
     return advantageValue
 }
