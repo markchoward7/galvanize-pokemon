5f8e648c2a608ecbd9960fa91945421cc3d6a07c
markchoward7 <markchoward7@outlook.com>
2020-06-18 10:59:13 -0500

    #17, colorized types

--- my_app_name/src/Pokemon.js
+++ my_app_name/src/Pokemon.js
@@ -11,6 +11,73 @@ class Pokemon extends React.Component {
         }
     }
 
+    componentDidUpdate() {
+        let typeHeaders = document.querySelectorAll(`.poke-card .type-name`)
+        let color = ""
+        for (const type of typeHeaders) {
+            switch (type.innerText) {
+                case "Bug":
+                    color = "#A8B820"
+                    break
+                case "Dark":
+                    color = "#705848"
+                    break
+                case "Dragon":
+                    color = "#7038F8"
+                    break;
+                case "Electric":
+                    color = "#F8D030"
+                    break;
+                case "Fairy":
+                    color = "#EE99AC"
+                    break
+                case "Fighting":
+                    color = "#C03028"
+                    break
+                case "Fire":
+                    color = "#F08030"
+                    break
+                case "Flying":
+                    color = "#A890F0"
+                    break
+                case "Ghost":
+                    color = "#705898"
+                    break
+                case "Grass":
+                    color = "#78C850"
+                    break
+                case "Ground":
+                    color = "#E0C068"
+                    break
+                case "Ice":
+                    color = "#98D8D8"
+                    break
+                case "Normal":
+                    color = "#A8A878"
+                    break
+                case "Poison":
+                    color = "#A040A0"
+                    break
+                case "Psychic":
+                    color = "#F85888"
+                    break
+                case "Rock":
+                    color = "#B8A038"
+                    break
+                case "Steel":
+                    color = "#B8B8D0"
+                    break
+                case "Water":
+                    color = "#6890F0"
+                    break
+                default:
+                    color = "#68A090"
+                    break
+            }
+            type.style.color = color
+        }
+    }
+
     handleCollection() {
         if (this.state.grandparent.state.collection.includes(this.state.pokemon)) {
             let newCollection = this.state.grandparent.state.collection
@@ -43,7 +110,7 @@ class Pokemon extends React.Component {
             <div className="poke-card grid-container-3">
                 <h4>{CapitalizeFirst(this.state.pokemon.name)}</h4>
                 {this.state.pokemon.types.length === 1 ? <div><h5 className="type-name" onClick={this.handleSearchType.bind(this)}>{CapitalizeFirst(this.state.pokemon.types[0].type.name)}</h5></div> : <div><h5 className="type-name" onClick={this.handleSearchType.bind(this)}>{CapitalizeFirst(this.state.pokemon.types[0].type.name)}</h5> <span>-</span> <h5 className="type-name" onClick={this.handleSearchType.bind(this)}>{CapitalizeFirst(this.state.pokemon.types[1].type.name)}</h5></div> }
-                {this.state.grandparent.state.looking_for_battle ? <button className="battle-button" onClick={this.handleBattle.bind(this)}>Battle</button> : ""}
+                {this.state.grandparent.state.looking_for_battle && this.state.grandparent.state.battle_pokemon.length < 2 ? <button className="battle-button" onClick={this.handleBattle.bind(this)}>Battle</button> : ""}
                 <div className="row-2-3">
                     <img src={this.state.pokemon.sprites.front_default} />
                 </div>
